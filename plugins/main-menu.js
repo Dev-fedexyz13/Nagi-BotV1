6e3cc75b06e87906b3aacab85958c1a7fe33fa58
⏤͟͟͞͞☆  𝑫𝒆𝒗-𝑩𝒍𝒂𝒄𝒌𝑫𝒂𝒓𝒌  ❀ <fedexyz13@gmail.com>
2025-10-25 09:41:01 -0300

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -1,9 +1,9 @@
-let handler = async (m, { conn, usedPrefix}) => {
+let handler = async (m, { conn, usedPrefix }) => {
   const delay = ms => new Promise(res => setTimeout(res, ms));
   const nombre = await conn.getName(m.sender);
   const user = global.db.data.users[m.sender];
 
-  const premium = user.premium? '✅ Sí': '❌ No';
+  const premium = user.premium ? '✅ Sí' : '❌ No';
   const limite = user.limit || 0;
   const totalreg = Object.keys(global.db.data.users).length;
   const groupsCount = Object.values(conn.chats).filter(v => v.id.endsWith('@g.us')).length;
@@ -14,21 +14,21 @@ let handler = async (m, { conn, usedPrefix}) => {
     const m = Math.floor((seconds % 3600) / 60);
     const s = Math.floor(seconds % 60);
     return [h, m, s].map(v => v.toString().padStart(2, '0')).join(':');
-}
+  }
 
   const infoUser = `
-🍃 *_Hola, ${nombre} Soy Nagi-BotV1._*
+🍃 *Hola, nombre. Soy Nagi-BotV1*
 
-🌿 *_INFO USUARIO_*
-> *_Usuario:_* *${nombre}*
-> *_Premium:_* *${premium}*
-> *_Límite:_* *${limite}*
+🌿 *INFO USUARIO*
+> Usuario: *{nombre}*
+> Premium: *premium*
+> Límite: *{limite}*
 
-🌿 *_DATOS DEL BOT_*
-> *_Grupos:_* *${groupsCount}*
-> *_Activo:_* *${muptime}*
-> *_Usuarios:_* *${totalreg}*
-> *_Plataforma:_* *Ubuntu*
+🌿 *DATOS DEL BOT*
+> Grupos: *groupsCount*
+> Activo: *{muptime}*
+> Usuarios: *${totalreg}*
+> Plataforma: *Ubuntu*
 `.trim();
 
   const imagen = 'https://dev-fedeexyz.vercel.app/media/mbe6sl.jpg';
@@ -37,31 +37,30 @@ let handler = async (m, { conn, usedPrefix}) => {
   await m.react('⚽');
 
   await conn.sendMessage(m.chat, {
-    image: { url: imagen },
-    caption: `${infoUser}\n\n${after}`,
-    contextInfo: {
-      externalAdReply: {
+  image:  url: imagen ,
+    caption: `{infoUser}\n\nafter`,
+    contextInfo: 
+      externalAdReply: 
         title: '© ᴍᴀᴅᴇ ʙʏ ᴅᴇᴠ-ꜰᴇᴅᴇxʏᴢ 🍂',
         body: '⌬ NagiBot-IA 🍃',
         thumbnailUrl: imagen,
         mediaType: 1,
         renderLargerThumbnail: true,
         sourceUrl: 'https://github.com/Dev-fedexyz13'
-}
-},
+      ,
     buttons: [
-      {
-        buttonId: `${usedPrefix}menucompleto`,
-        buttonText: { displayText: '🌿 ᴍᴇɴᴜ ᴄᴏᴍᴘʟᴇᴛᴏ'},
+      
+        buttonId: `{usedPrefix}menucompleto`,
+        buttonText: { displayText: '🌿 ᴍᴇɴᴜ ᴄᴏᴍᴘʟᴇᴛᴏ' },
         type: 1
-},
+      },
       {
         buttonId: `${usedPrefix}reg Nagi-Bot.17`,
-        buttonText: { displayText: '🌿 ᴀᴜᴛᴏ ʀᴇɢɪꜱᴛʀᴏ'},
+        buttonText: { displayText: '🌿 ᴀᴜᴛᴏ ʀᴇɢɪꜱᴛʀᴏ' },
         type: 1
-}
+      }
     ]
-}, { quoted: m});
+  }, { quoted: m });
 
   await delay(400);
 };
